d4ebfa7b5f4fb0c3683b07150432d86c34901d7c
Luís M. R. Guimarães <100025288+luismrguimaraes@users.noreply.github.com>
2024-05-01 19:25:29 +0100

    add all notes off and all sounds off CCs support (#3)

--- include/sst/voicemanager/voicemanager.h
+++ include/sst/voicemanager/voicemanager.h
@@ -187,6 +187,13 @@ template <typename Cfg, typename Responder> struct VoiceManager
         return 0;
     }
 
+    void allNotesOff(){
+        responder.allNotesOff();
+    }
+    void allSoundsOff(){
+        responder.allSoundsOff();
+    }
+
     static float midiToFloatVelocity(uint8_t vel) { return 1.f * vel / 127.f; }
 
     using polymanager_t = sst::voicemanager::managers::PolyManager<Cfg, Responder>;
